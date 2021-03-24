e4cd6323f72c90742f14298aeb280e7b56acf538
hujiejeff <hujiejeff@163.com>
2021-03-24 18:01:28 +0800

    [feature] 添加接口

--- module/common/src/main/java/network/WanAndroidApi.kt
+++ module/common/src/main/java/network/WanAndroidApi.kt
@@ -2,9 +2,7 @@ package network
 
 import com.hujiejeff.wanadnroid.module.base.data.remote.bean.BaseBean
 import network.bean.*
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface WanAndroidApi {
     //首页
@@ -63,11 +61,82 @@ interface WanAndroidApi {
     @GET("/article/list/0/json?author=鸿洋")
     suspend fun searchArticlesByAuthor(): BaseBean<PageBean<ArticleBean>>
 
-//    //导航
-//    @GET("/navi/json")
-//    suspend fun
+    /**
+     * 导航
+     */
+    @GET("/navi/json")
+    suspend fun getAllNaviArticles(): BaseBean<NaviBean>
+
+    /**
+     *  项目分类
+     */
+    @GET("/project/tree/json")
+    suspend fun getProjectTree(): BaseBean<List<TreeBean>>
+
+    /**
+     * 某个分类下的项目
+     */
+    @GET("/project/list/1/json?cid=294")
+    suspend fun getProjectsByCid(@Query("cid") cid: Int): BaseBean<PageBean<ArticleBean>>
+
     //问答
 
 
     //个人
+
+    /**
+     * 登录
+     */
+    @FormUrlEncoded
+    @POST("/user/login")
+    suspend fun login(
+        @Field("username") username: String,
+        @Field("password") password: String
+    ): BaseBean<UserBean>
+
+    /**
+     * 注册
+     */
+    @FormUrlEncoded
+    @POST("/user/register")
+    suspend fun register(
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Field("repassword") rePassword: String
+    ): BaseBean<Any>
+
+    /**
+     * 退出登录
+     */
+    @GET("/user/logout/json")
+    suspend fun logout()
+
+
+    //收藏
+
+    /**
+     * 收藏文章列表
+     */
+    @GET("/lg/collect/list/{page}/json")
+    suspend fun getCollectArticles(@Path("page") page: Int): BaseBean<PageBean<ArticleBean>>
+
+    /**
+     * 收藏站内文章
+     */
+    @POST("https://www.wanandroid.com/lg/collect/{id}/json")
+    suspend fun collectInnerArticle(@Path("id") id: Int)
+
+    /**
+     * 收藏站外文章
+     */
+    @FormUrlEncoded
+    @POST("/lg/collect/add/json")
+    suspend fun collectOutArticle(@Field("title") title: String, @Field("author") author: String)
+
+    /**
+     * 取消收藏
+     */
+    @POST("/lg/uncollect_originId/{id}/json")
+    suspend fun unCollectArticle()
+
 }
---
+++ module/common/src/main/java/network/bean/NaviBean.kt
@@ -0,0 +1,7 @@
+package network.bean
+
+data class NaviBean(
+    val articles: List<ArticleBean>,
+    val cid: Int,
+    val name: String
+)
