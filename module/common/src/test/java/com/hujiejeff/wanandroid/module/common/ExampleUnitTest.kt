8bd211e818f409b14e7405d54fd257ef31dc8b2e
hujiejeff <hujiejeff@163.com>
2022-09-22 09:16:37 +0800

    协程练习代码

--- module/common/src/test/java/com/hujiejeff/wanandroid/module/common/ExampleUnitTest.kt
+++ module/common/src/test/java/com/hujiejeff/wanandroid/module/common/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.hujiejeff.wanandroid.module.common
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import network.DataModel
 import org.junit.Test
 
@@ -19,7 +19,49 @@ class ExampleUnitTest {
 
     @Test
     fun testApi() = runBlocking {
-        val rep = DataModel.getMainArticles(0)
-        println(rep)
+        val coroutineScopeA = CoroutineScope(Dispatchers.Default)
+
+            coroutineScope {
+                val rep = DataModel.getMainArticles(0)
+                println(rep)
+            }
+            println("end")
+
+        println("over")
+        withContext(Dispatchers.Default) {}
+        val result = async {
+            delay(1000)
+            2
+        }
+        println("My job is ${coroutineContext[Job]}")
+        async {
+
+        }
+        joinAll()
     }
+
+
+    @Test
+    fun test() = runBlocking {
+       test1()
+    }
+}
+
+fun test1() = runBlocking { // this: CoroutineScope
+    launch {
+        delay(200L)
+        println("Task from runBlocking")
+    }
+
+    coroutineScope { // 创建一个协程作用域
+        launch {
+            delay(500L)
+            println("Task from nested launch")
+        }
+
+        delay(100L)
+        println("Task from coroutine scope") // 这一行会在内嵌 launch 之前输出
+    }
+
+    println("Coroutine scope is over") // 这一行在内嵌 launch 执行完毕后才输出
 }
