befb272b7a828901b6be7a0382967d5ff693d21a
hujiejeff <hujiejeff@163.com>
2022-10-18 10:59:39 +0800

    添加webview

---
+++ module/base/src/main/java/com/hujiejeff/wanadnroid/module/base/webview/WebViewFragment.kt
@@ -0,0 +1,11 @@
+package com.hujiejeff.wanadnroid.module.base.webview
+
+import com.hujiejeff.wanadnroid.module.base.base.BaseFragment
+import com.hujiejeff.wanadnroid.module.base.databinding.FragmentWebviewBinding
+import kotlinx.android.synthetic.main.fragment_webview.*
+
+class WebViewFragment: BaseFragment<FragmentWebviewBinding>() {
+    init {
+
+    }
+}
